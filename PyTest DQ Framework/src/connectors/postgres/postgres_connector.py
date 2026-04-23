cea3f9025b1b459def867e9b74b3595e52170839
rishabhjhs56 <49242674+rishabhjhs56@users.noreply.github.com>
2026-04-23 17:34:45 +0530

    Update postgres_connector.py

--- PyTest DQ Framework/src/connectors/postgres/postgres_connector.py
+++ PyTest DQ Framework/src/connectors/postgres/postgres_connector.py
@@ -1,17 +1,26 @@
-
 import psycopg2
 
+
 class PostgresConnectorContextManager:
-    def __init__(self, db_host: str, db_name: str, ...):
-        # init
+
+    def __init__(self, db_host, db_port, db_name, db_user, db_password):
+        self.db_host = db_host
+        self.db_port = db_port
+        self.db_name = db_name
+        self.db_user = db_user
+        self.db_password = db_password
+        self.connection = None
 
     def __enter__(self):
-        # create conn
+        self.connection = psycopg2.connect(
+            host=self.db_host,
+            port=self.db_port,
+            database=self.db_name,
+            user=self.db_user,
+            password=self.db_password
+        )
+        return self.connection
 
-    def __exit__(self, exc_type, exc_value, exc_tb):
-        # close conn
-
-    def get_data_sql(self, sql):
-        # exec query, result = pandas df
-
-
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.connection:
+            self.connection.close()

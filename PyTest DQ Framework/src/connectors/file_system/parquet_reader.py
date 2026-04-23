40b5ffc8a5a4d63bafbf02fbbf0377400ff67d13
rishabhjhs56 <49242674+rishabhjhs56@users.noreply.github.com>
2026-04-23 19:59:43 +0530

    Update parquet_reader.py

--- PyTest DQ Framework/src/connectors/file_system/parquet_reader.py
+++ PyTest DQ Framework/src/connectors/file_system/parquet_reader.py
@@ -1,11 +1,13 @@
 import pandas as pd
+import pytest
 
 
-def parquet_reader(request):
-    try:
-        reader = ParquetReader()
-        yield reader
-    except Exception as e:
-        pytest.fail(f"Failed to initialize ParquetReader: {e}")
-    finally:
-        del reader
+class ParquetReader:
+    def parquet_reader(request):
+        try:
+            reader = ParquetReader()
+            yield reader
+        except Exception as e:
+            pytest.fail(f"Failed to initialize ParquetReader: {e}")
+        finally:
+            del reader

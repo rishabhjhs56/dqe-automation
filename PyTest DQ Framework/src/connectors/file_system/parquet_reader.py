6aa2548cd7861696e480bbc3dad4cfed666914e2
rishabhjhs56 <49242674+rishabhjhs56@users.noreply.github.com>
2026-04-27 13:53:45 +0530

    Update parquet_reader.py

--- PyTest DQ Framework/src/connectors/file_system/parquet_reader.py
+++ PyTest DQ Framework/src/connectors/file_system/parquet_reader.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
 class ParquetReader:
+
     def __init__(self):
         pass
 
     def process(self, file_path):
-        # Reads a Parquet file and returns a DataFrame
         try:
-            return pd.read_parquet(file_path)
+            return pd.read_parquet(file_path, engine="pyarrow")
         except Exception as e:
             raise RuntimeError(f"Failed to read parquet file: {e}")
+            
